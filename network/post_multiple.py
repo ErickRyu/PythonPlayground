f4a491c50067a0bfe339f161607daa3fb935d132
woong-innosonian <woong@innosonian.com>
2019-01-03 19:03:21 +0900

    address

--- network/post_multiple.py
+++ network/post_multiple.py
@@ -1,35 +1,23 @@
 import json
 import requests, pprint
 
-url = 'https://miffle.braydenlab.com/miffle/file_get_multiple.php'
-#url = 'https://miffle.braydenlab.com/miffle/post_php.php'
-
-#headers = {'Content-type': 'multipart/form-data', 'charset': 'UTF-8'}
-#headers = {'content-type': 'application/binary'}
-#headers = {'Content-type': 'application/json'}
+# url = 'https://miffle.braydenlab.com/miffle/file_get_multiple.php'
+# url = 'https://miffle.braydenlab.com/miffle/post_php.php'
+url = 'https://miffle.braydenlab.com/miffle/get_files_and_calc.php'
 
 f_json = open('train_condition.json', 'rb')
-json_file = json.load(f_json)
-#print(type(json_file))
-
 f_bin = open('rawHexBPfile.bin', 'rb')
 
-#files = {'rawHexBPfile': f_bin}
-
-sample = {'a': {'b':'c'}}
-
+json_file = json.load(f_json)
 files = {
-    'json': (None, json.dumps(sample), 'application/json'),
+    'json': (None, json.dumps(json_file), 'application/json'),
     'rawHexBPfile': ('rawHexBPfile.bin', f_bin, 'application/octet-stream')
 }
 
-
-#print(type(sample_json))
-
-#response = requests.post(url, data=sample, files=files, headers=headers)
-#response = requests.post(url, data=json_file, files=files)
 response = requests.post(url, files=files)
 
 print(response.url)
 pprint.pprint(response.text)
+
+#print(response.json())
 
