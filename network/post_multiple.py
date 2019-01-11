7a37571bced59c4cc38ed15432e22a03dec2f543
woong-innosonian <woong@innosonian.com>
2019-01-11 16:25:21 +0900

    add openskill 1.6

--- network/post_multiple.py
+++ network/post_multiple.py
@@ -3,14 +3,15 @@ import requests, pprint
 
 # url = 'https://miffle.braydenlab.com/miffle/file_get_multiple.php'
 # url = 'https://miffle.braydenlab.com/miffle/post_php.php'
-url = 'https://miffle.braydenlab.com/miffle/get_files_and_calc.php'
+# url = 'https://miffle.braydenlab.com/miffle/get_files_and_calc.php'
+url = 'https://toffle.braydenlab.com/leuven/post_php.php'
 
 f_json = open('train_condition.json', 'rb')
 f_bin = open('rawHexBPfile.bin', 'rb')
 
 json_file = json.load(f_json)
 files = {
-    'json': (None, json.dumps(json_file), 'application/json'),
+    'data': (None, json.dumps(json_file), 'application/json'),
     'rawHexBPfile': ('rawHexBPfile.bin', f_bin, 'application/octet-stream')
 }
 
