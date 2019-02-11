fc96f3314a8290f7ef9a3aae0fb82e1862d72c5d
Woong <hwoong@gmail.com>
2019-02-11 23:33:02 +0900

    test

--- datetime/epoch.py
+++ datetime/epoch.py
@@ -3,3 +3,5 @@ import time
 print(time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.localtime(1545305037)))
 
 print(time.strftime("%Y-%m-%d %H:%M:%S +0000", time.localtime(1545305037)))
+
+print(1)
