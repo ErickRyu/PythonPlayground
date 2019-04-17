077921cdee168b3c1824bc601c32821c1f11de2b
woong-innosonian <woong@innosonian.com>
2019-04-17 18:54:43 +0900

    BP_bin_view

--- datetime/epoch.py
+++ datetime/epoch.py
@@ -1,7 +1,19 @@
+import os
 import time
 
-print(time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.localtime(1545305037)))
 
-print(time.strftime("%Y-%m-%d %H:%M:%S +0000", time.localtime(1545305037)))
+def print_time():
+    print(time.strftime("%Y-%m-%d %H:%M:%S +0000", time.localtime(1554975584)))
 
-print(1)
+
+print(time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.localtime(1554975584)))
+
+os.environ['TZ'] = 'US/Eastern'
+print_time()
+#print(time.strftime("%Y-%m-%d %H:%M:%S +0000", time.localtime(1554975584)))
+
+os.environ['TZ'] = 'Europe/Brussels'
+print_time()
+#print(time.strftime("%Y-%m-%d %H:%M:%S +0000", time.localtime(1554975584)))
+
+
