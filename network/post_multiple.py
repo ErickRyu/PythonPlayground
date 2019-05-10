55d3e03c954ee66b129b7b6c6bb61649a8940dc6
woong-innosonian <woong@innosonian.com>
2019-05-10 10:52:24 +0900

    re-post test

--- network/post_multiple.py
+++ network/post_multiple.py
@@ -1,5 +1,6 @@
 import json
-import requests, pprint
+import requests
+import time
 
 # url = 'https://miffle.braydenlab.com/miffle/file_get_multiple.php'
 # url = 'https://miffle.braydenlab.com/miffle/post_php.php'
@@ -12,9 +13,15 @@ import requests, pprint
 #url ='https://general.braydenlab.com/port/cpr_analyzer_provider_genk'
 url ='https://hstream.braydenlab.com/hstream/cpr_analyze_provider.php'
 
-f_json = open('train_condition.json', 'rb')
-f_bin = open('rawHexBPfile.bin', 'rb')
+rawHexBPfile = 'bigsize_rawHexBPfile.bin'
 
+#f_json = open('bigsize_train_condition.json', 'rb')
+#f_bin = open('bigsize_rawHexBPfile.bin', 'rb')
+# f_json = open('normal_train_condition.json', 'rb')
+# f_bin = open('normal_rawHexBPfile.bin', 'rb')
+
+f_json = open('normal_train_condition.json', 'rb')
+f_bin = open(rawHexBPfile, 'rb')
 json_file = json.load(f_json)
 files = {
     'data': (None, json.dumps(json_file), 'application/json'),
@@ -22,7 +29,39 @@ files = {
 }
 
 response = requests.post(url, files=files)
-
 print(response.url)
-#pprint.pprint(response.text)
 print(response.text)
+# time.sleep(0.1)
+
+f_bin = open(rawHexBPfile, 'rb')
+files = {
+    'data': (None, json.dumps(json_file), 'application/json'),
+    'rawHexBPfile': ('rawHexBPfile.bin', f_bin, 'application/octet-stream')
+}
+
+response = requests.post(url, files=files)
+print(response.url)
+print(response.text)
+
+if response.text == '':
+    print('--------------- None response --------------')
+
+    f_bin = open(rawHexBPfile, 'rb')
+    files = {
+        'data': (None, json.dumps(json_file), 'application/json'),
+        'rawHexBPfile': ('rawHexBPfile.bin', f_bin, 'application/octet-stream')
+    }
+
+    response = requests.post(url, files=files)
+    print(response.url)
+    print(response.text)
+
+# f_bin = open(rawHexBPfile, 'rb')
+# files = {
+#     'data': (None, json.dumps(json_file), 'application/json'),
+#     'rawHexBPfile': ('rawHexBPfile.bin', f_bin, 'application/octet-stream')
+# }
+#
+# response = requests.post(url, files=files)
+# print(response.url)
+# print(response.text)
