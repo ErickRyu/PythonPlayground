766b34f8696bba808125848f8bcee9bdfc0d1954
Woong <hwoong@gmail.com>
2019-05-20 22:35:48 +0900

    add q3

---
+++ StudyPieWebCrawling/2nd_week/q3_count_in_xml.py
@@ -0,0 +1,19 @@
+import urllib.request
+import ssl
+import xml.etree.ElementTree as ET
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+data = urllib.request.urlopen('http://python-data.dr-chuck.net/comments_42.xml', context=ctx).read()
+xml_file = data.decode('utf-8')
+
+stuff = ET.fromstring(xml_file)
+lst = stuff.findall('comments/comment')
+
+sum_of_count = 0
+for item in lst:
+    sum_of_count += int(item.find('count').text)
+
+print('Sum of count', sum_of_count)
